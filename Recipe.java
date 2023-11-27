7cc482396a6f16608d9dcc7a14d8ff6aa373fb9a
saraelbana <150337030+saraelbana@users.noreply.github.com>
2023-11-27 10:55:51 +0100

    pushing project main basic skeleton

--- Recipe.java
+++ Recipe.java
@@ -1,5 +1,8 @@
 public class Recipe {
+    /* constructors*/
     public Recipe(){}
   //ArrayList<Type> str = new ArrayList<Type>;
-   // public Recipe(Ingredient ){}
+   public Recipe(String name){}
+    private String name; // recipe name
+    private String category; // appertizer, main dish, side dish or dessert
 }
